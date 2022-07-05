cfd8a6cf928d4eb2193e5036349304fd271d914c
«Aleksandr» <navalniu202@gmail.com>
2022-07-05 12:29:19 +0300

    HomeWork001

---
+++ HomeWork001/Program.cs
@@ -0,0 +1,19 @@
+﻿int SumNumber(int value)
+{
+          int result = 0;
+          while (value > 0)
+          {
+               result+= value % 10;
+               value = value / 10;     
+          }
+          return result;                          
+}
+
+Console.Write("Введите число - ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = SumNumber(num);
+
+Console.WriteLine(result);
+
